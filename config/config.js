2f287978ac84ec36256807b1750665eec69fc44e
Aaron TSOU <huichiao.tsou@gmail.com>
2023-02-21 17:17:36 +0800

    add error log to missing config

--- config/config.js
+++ config/config.js
@@ -7,6 +7,12 @@ try {
   const path = process.cwd() + "/config/config.yaml";
   config = yaml.load(fs.readFileSync(path, "utf8"));
 } catch (e) {
+  if (e.toString().includes("no such file or directory")) {
+    console.debug(
+      "config.yaml file is not found,\nmake sure you add it under the /config directory"
+    );
+    process.exit();
+  }
   console.log(e);
 }
 
