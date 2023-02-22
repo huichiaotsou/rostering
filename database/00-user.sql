69049aebe5df8ebdcade3ee027db42fb97d8c9ee
huichiaotsou <huichiao.tsou@gmail.com>
2023-02-22 22:30:02 +0800

    create db schema

--- database/00-user.sql
+++ database/00-user.sql
@@ -1,39 +1,49 @@
--- Define the access table to define the access level of the user (permissions): leader, volunteer, etc.
-CREATE TABLE access (
-    id                  INT          NOT NULL AUTO_INCREMENT,
-    description         VARCHAR(50)  NOT NULL DEFAULT 'no_description_given',
-    PRIMARY KEY (id)
-);
-
 -- Define the user table to store the user's basic information
 CREATE TABLE user (
-    id                  INT          NOT NULL AUTO_INCREMENT,
+    id                  INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name          VARCHAR(50)  NOT NULL,
+    last_name           VARCHAR(50)  NOT NULL,
     email               VARCHAR(255) NOT NULL UNIQUE,
-    name                VARCHAR(50)  NOT NULL,
-    pwd_hash_or_token   TEXT         NOT NULL,
-    access_id           INT          NOT NULL,
-
-    PRIMARY KEY (id),
-    FOREIGN KEY (access_id) REFERENCES access(id),
-    INDEX (access_id)
+    email_verified      BOOLEAN      NOT NULL DEFAULT false,
+    pwd_hash_or_token   TEXT         NOT NULL, -- password hash or OAuth login token
+    date_of_birth       DATE         NULL,
+    create_date         DATE         NOT NULL,
 );
 
--- Define the func table to store the functions of the users: vox, EG, KB ...
+-- Define the team table to store the teams of the users: worship, sound...
+CREATE TABLE team (
+    id          INT         AUTO_INCREMENT PRIMARY KEY,
+    team_name   VARCHAR(50) NOT NULL
+);
+
+-- Define the user_team table to indicate WHO is in WHICH TEAM
+-- 1 user can be in more than 1 team
+CREATE TABLE user_team (
+    user_id    INT NOT NULL REFERENCES user(id),
+    team_id    INT NOT NULL REFERENCES team(id),
+    UNIQUE (user_id, team_id)
+);
+
+-- Define the access table to store the access level along with team(s)
+CREATE TABLE access (
+    id              INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    team_id         INT          NOT NULL REFERENCES team(id),
+    user_id         INT          NOT NULL REFERENCES user(id),
+    access_level    VARCHAR(50)  NOT NULL
+);
+
+-- Define the func table to store the functions of the users: Vox 1, Vox 2, KB ...
 CREATE TABLE func (
-    id      INT          NOT NULL AUTO_INCREMENT,
-    name    VARCHAR(50)  NOT NULL,
+    id          INT          NOT NULL AUTO_INCREMENT,
+    func_name   VARCHAR(50)  NOT NULL,
     PRIMARY KEY (id)
 );
 
--- Define the roles table to indicate WHO can be in charge of WHAT(1 user can play more than 1 instrument)
-CREATE TABLE roles (
-    id         INT AUTO_INCREMENT PRIMARY KEY,
-    user_id    INT NOT NULL,
-    func_id    INT NOT NULL,
-
-    FOREIGN KEY (user_id) REFERENCES user(id),
-    FOREIGN KEY (func_id) REFERENCES func(id),
-    INDEX (user_id),
-    INDEX (func_id),
-    UNIQUE KEY (user_id, func_id)
+-- Define the user_func table to indicate WHO can be in charge of WHAT
+-- 1 user can be in charge of more than 1 instrument/function
+-- (who can play what is defined by the admins)
+CREATE TABLE user_func (
+    user_id    INT NOT NULL REFERENCES user(id),
+    func_id    INT NOT NULL REFERENCES func(id),
+    UNIQUE (user_id, func_id)
 );
--- database/01-service.sql
+++ database/01-service.sql
@@ -1,1 +1,53 @@
--- service time, rehearsal time, call time
+-- This table stores different service types
+CREATE TABLE service_type (
+    id                  INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    service_name        VARCHAR(50)     NOT NULL, -- Sunday Service, Baptism, Creative Team Night, Heart and Soul
+
+    service_day         VARCHAR(20)     NOT NULL, -- Sunday, Saturday, Tuesday
+    
+    call_time           TIME            NOT NULL DEFAULT "00:00:00",
+    call_time_day       VARCHAR(50)     NOT NULL DEFAULT "", -- e.g. Sunday
+    
+    preparation_time    TIME            NOT NULL DEFAULT "00:00:00",
+    preparation_day     VARCHAR(50)     NOT NULL DEFAULT "", -- e.g. Saturday
+
+    service_time_start  TIME            NOT NULL DEFAULT "07:00:00",
+    service_time_end    TIME            NOT NULL DEFAULT "15:45:00",
+
+    team_id             INT             NOT NULL REFERENCES team(id), -- who owns the service/event
+    notes               TEXT            NOT NULL
+);
+
+-- "service_func" stores the required functions of a service,
+-- it will be used with "service_type" to generate the new schedule tables for rostering
+-- ref. rostering_sunday table
+CREATE TABLE service_func (
+    service_type_id     INT         NOT NULL    REFERENCES service_type(id),
+    func_id             INT         NOT NULL    REFERENCES func(id),
+    is_mandatory        BOOLEAN     NOT NULL    DEFAULT true,
+    
+    UNIQUE (service_type_id, func_id)
+);
+
+-- This table stores all the services that need to be rostered,
+-- creation of services is periodically triggered by admins
+CREATE TABLE services (
+    id                  INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    service_type_id     INT             NOT NULL REFERENCES service_type(id),
+    service_date        DATE            NOT NULL,
+
+    UNIQUE(service_type_id, servive_date)
+);
+
+-- This table stores the availability of volunteers
+CREATE TABLE availability (
+    user_id                 INT     NOT NULL REFERENCES user(id),
+    service_id              INT     NOT NULL REFERENCES services(id),
+);
+
+CREATE TABLE max_this_month (
+    user_id         INT     NOT NULL REFERENCES user(id),
+    year            INT     NOT NULL, -- 2023, 2024 ...
+    month           INT     NOT NULL, -- 1, 2, 3, 4, 5 ...
+    max_this_month  INT     NOT NULL
+);
