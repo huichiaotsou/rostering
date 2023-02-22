0a012c2e69747224f8369e708cc6b1d98ed55f26
huichiaotsou <huichiao.tsou@gmail.com>
2023-02-22 13:17:08 +0800

    refine sql file with gpt

--- database/00-user.sql
+++ database/00-user.sql
@@ -1,38 +1,39 @@
--- access table defines the access level of the user (permissions): leader, volunteer, etc.
-CREATE TABLE access(
-    id                  INT     NOT NULL AUTO_INCREMENT,
-    access_description  TEXT    NOT NULL,
-    
+-- Define the access table to define the access level of the user (permissions): leader, volunteer, etc.
+CREATE TABLE access (
+    id                  INT          NOT NULL AUTO_INCREMENT,
+    description         VARCHAR(50)  NOT NULL DEFAULT 'no_description_given',
     PRIMARY KEY (id)
 );
 
--- user table stores the user's basic information
+-- Define the user table to store the user's basic information
 CREATE TABLE user (
-    id        INT     NOT NULL AUTO_INCREMENT,
+    id                  INT          NOT NULL AUTO_INCREMENT,
+    email               VARCHAR(255) NOT NULL UNIQUE,
+    name                VARCHAR(50)  NOT NULL,
+    pwd_hash_or_token   TEXT         NOT NULL,
+    access_id           INT          NOT NULL,
+
     PRIMARY KEY (id),
-
-    email     TEXT    NOT NULL UNIQUE,
-    name      TEXT    NOT NULL,
-    password  TEXT    NOT NULL,
-
-    access_id   INT     NOT NULL,
-    FOREIGN KEY (access_id) REFERENCES access(id)
+    FOREIGN KEY (access_id) REFERENCES access(id),
+    INDEX (access_id)
 );
 
--- func table stores the functions of the users: vox, EG, KB ...
+-- Define the func table to store the functions of the users: vox, EG, KB ...
 CREATE TABLE func (
-    id      INT     NOT NULL AUTO_INCREMENT,
-    func    TEXT    NOT NULL,
-
+    id      INT          NOT NULL AUTO_INCREMENT,
+    name    VARCHAR(50)  NOT NULL,
     PRIMARY KEY (id)
 );
 
--- roles table indicates WHO can be in charge of WHAT(1 user can play more than 1 instrument)
+-- Define the roles table to indicate WHO can be in charge of WHAT(1 user can play more than 1 instrument)
 CREATE TABLE roles (
-    user_id     INT     NOT NULL,
-    func_id     INT     NOT NULL,
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    user_id    INT NOT NULL,
+    func_id    INT NOT NULL,
 
     FOREIGN KEY (user_id) REFERENCES user(id),
-    FOREIGN KEY (func_id) REFERENCES func(id)
+    FOREIGN KEY (func_id) REFERENCES func(id),
+    INDEX (user_id),
+    INDEX (func_id),
+    UNIQUE KEY (user_id, func_id)
 );
-
